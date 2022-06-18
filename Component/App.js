446519e086c48e102e8284bcd61ca37786326bdd
YhinkaDevOps <102762203+YhinkaDevOps@users.noreply.github.com>
2022-06-18 18:49:32 +0100

    Add files via upload

---
+++ Component/App.js
@@ -0,0 +1,11 @@
+import Header from "./Component/Header"
+const App = () =>{
+
+  return (
+    <div className="container">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Component/Header.js
@@ -0,0 +1,17 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h1 style={headingStyle} className="header-class">Basic react application</h1>
+    </div>
+  )
+}
+
+const headingStyle = {
+  color:'orange',
+  marginTop: '9em',
+  marginLeft: '15em',
+  marginRight: '10em',
+}
+
+export default Header
